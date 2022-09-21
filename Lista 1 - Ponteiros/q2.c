f1fa049ab140abc9e4d0f948a5c9406d8eda6255
AndrGordShow <andredecampos.ac@gmail.com>
2022-09-21 20:48:30 -0300

    Questao 2 feita

--- Lista 1 - Ponteiros/q2.c
+++ Lista 1 - Ponteiros/q2.c
@@ -9,15 +9,23 @@ int main(){
     float raio, perimetro, area;
     float *pPerimetro, *pArea;
 
-    printf("Digite o raio: ");
-    scanf();
+    pPerimetro = &perimetro;
+    pArea = &area; 
 
+    printf("Digite o raio: ");
+    scanf("%f", &raio);
+
+    calcula_circulo(raio, pPerimetro, pArea);
+
+    printf("O perimetro é: %.2f\n", *pPerimetro);
+    printf("A area é: %.2f\n", *pArea);
     
     return 0;
 }
 
 void calcula_circulo(float raio, float *pPerimetro, float *pArea){
     
-    
+    *pPerimetro = (raio*2)*3.1415926;
 
+    *pArea = (raio*raio)*3.1415926;
 }
---
+++ Lista 1 - Ponteiros/q3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void cacula_hora(int totalMinutos, int *ph, int *pm);
+
+int main(){
+
+    
+    
+    return 0;
+}
+
+void cacula_hora(int totalMinutos, int *ph, int *pm){
+
+}
